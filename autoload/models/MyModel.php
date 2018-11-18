1633ffc3eb165ad7c0b37daf31e98fc6e6285e2d
giito <giito0101@gmail.com>
2018-11-18 20:18:20 +0900

    autoload add

---
+++ autoload/models/MyModel.php
@@ -0,0 +1,11 @@
+<?php
+#modelsという名前空間の宣言が必要
+namespace Models;
+
+class MyModel
+{
+	public function getHello()
+	{
+		return 'Hello !! You well done !!' . PHP_EOL;
+	}
+}
---
+++ autoload/models/sub/SubModel.php
@@ -0,0 +1,11 @@
+<?php
+# Models\Subという名前空間の宣言が必要
+namespace Models\Sub;
+
+class SubModel
+{
+	public function getSubHello()
+	{
+		return 'Wow !! You got another Hello in subModel. Well done !! ' . PHP_EOL;
+	}
+}
